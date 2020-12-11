da99dc3299cbaca74f2680fc26ef779566e32e87
Pedro Pires <pcaetanop@gmail.com>
2020-12-11 11:35:09 -0300

    Creating file

---
+++ src/components/Nickname.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="HeaderContainer">
+    <div class="NickNameContainer">
+      <h3 class="NickName">Drull</h3>
+      <span class="Tag">#BR1</span>
+    </div>
+    <p class="Session">SIGN OUT</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nickname",
+};
+</script>
+
+<style scoped>
+.NickNameContainer {
+  display: flex;
+}
+.NickName {
+  color: rgb(255, 255, 255);
+  font-size: 1rem;
+  font-weight: normal;
+}
+.Tag {
+  padding-left: 0.3rem;
+  color: #aaaaaa;
+}
+.Session {
+  font-size: 0.57rem;
+  color: rgb(64, 207, 233);
+  letter-spacing: -0.4px;
+}
+</style>
---
+++ src/components/Play.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="Main">
+    <div class="ImageContainer">
+      <img
+        class="Exclamation"
+        src="../assets/exclamation.svg"
+        alt="Exclamation"
+      />
+    </div>
+    <div class="TextContainer">
+      <div class="MainText">
+        <h1 class="Play">JOGAR</h1>
+      </div>
+      <img class="PlayIcon" src="../assets/play.svg" alt="PlayIcon" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Play",
+};
+</script>
+
+<style scoped>
+.Main {
+  display: flex;
+  padding-top: 0.5rem;
+}
+.Exclamation {
+  width: 3rem;
+  margin-left: -0.3rem;
+}
+.TextContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+.MainText {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+}
+
+.Play {
+  font-size: 18rem;
+  color: white;
+  letter-spacing: -1rem;
+}
+h1:hover {
+  color: #1dcdda;
+}
+.PlayIcon {
+  height: 15rem;
+  padding-left: 1rem;
+}
+</style>
