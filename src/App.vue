a6b343bf32aa79444b7db9354b563e991ef868c0
Pedro Pires <pcaetanop@gmail.com>
2020-12-11 11:33:42 -0300

    Adding App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+    <Nickname/>
+    <Play/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nickname from './components/Nickname.vue'
+import Play from './components/Play.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Nickname,
+    Play
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@600;900&display=swap');
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  font-family: 'Poppins', sans-serif;
 }
+html {
+  background: linear-gradient(rgba(22, 22, 22, 0.76), rgba(22, 22, 22, 0.76)), url("./assets/Login_image.jpg");
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center;
+}
+.container{
+  margin: 3rem;
+}
+
 </style>
