7b5423e3f555254f39a1b645122e78f231664870
Pedro Pires <pcaetanop@gmail.com>
2020-12-11 17:46:18 -0300

    Creating Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="Main">
+    <div class="LogoContainer">
+      <div class="LogoGroup">
+      <img class="Riot" src="../assets/riot.png" alt="Riot logo">
+      <img class="Lol" src="../assets/lol.png" alt="Logo">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+.LogoContainer{
+  margin-top: 2rem;
+}
+.LogoGroup{
+  display: flex;
+  align-items: center;
+}
+.Lol{
+  width: 15rem;
+  border-left: 0.1rem solid #b4b4b4be;
+  
+}
+.Riot{
+  width: 5rem;
+  margin-right: 0.5rem
+}
+
+</style>
