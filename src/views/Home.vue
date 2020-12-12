20e1e2a4826d772def12eb5d09e58d2f58f3893d
Pedro Pires <pcaetanop@gmail.com>
2020-12-12 15:09:50 -0300

    Creating files

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <router-view />
+      <Nickname />
+      <Play />
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import Nickname from "../components/Nickname";
+import Play from "../components/Play.vue";
+import Footer from "../components/Footer.vue";
+export default {
+  name: "App",
+  components: {
+    Nickname,
+    Play,
+    Footer,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@600;900&display=swap");
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  font-family: "Poppins", sans-serif;
+}
+html {
+  background: linear-gradient(rgba(22, 22, 22, 0.76), rgba(22, 22, 22, 0.76)),
+    url("../assets/Login_image.jpg");
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center;
+}
+.container {
+  margin: 3rem;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Login page being built</h1>
+  </div>
+</template>
