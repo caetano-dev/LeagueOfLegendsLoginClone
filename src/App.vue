96e5c7eacca1bf5ca731dcbbbd1767357d1316bd
Pedro Pires <pcaetanop@gmail.com>
2020-12-12 15:09:21 -0300

    Refactoring for router

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,35 @@
 <template>
   <div id="app">
     <div class="container">
-    <Nickname/>
-    <Play/>
-    <Footer/>
     </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-import Nickname from './components/Nickname.vue'
-import Play from './components/Play.vue'
-import Footer from './components/Footer.vue'
-
 export default {
-  name: 'App',
-  components: {
-    Nickname,
-    Play,
-    Footer
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@600;900&display=swap');
-*{
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@600;900&display=swap");
+* {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
   text-decoration: none;
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 html {
-  background: linear-gradient(rgba(22, 22, 22, 0.76), rgba(22, 22, 22, 0.76)), url("./assets/Login_image.jpg");
+  background: linear-gradient(rgba(22, 22, 22, 0.76), rgba(22, 22, 22, 0.76)),
+    url("./assets/Login_image.jpg");
   background-size: cover;
   background-attachment: fixed;
   background-position: center;
 }
-.container{
+.container {
   margin: 3rem;
 }
-
 </style>
