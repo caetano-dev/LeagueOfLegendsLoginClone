d5e430525205d55d6566792ebe5e419d2ff42fe7
Pedro Pires <pcaetanop@gmail.com>
2020-12-13 13:35:13 -0300

    Removing styles

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <div class="container">
-    </div>
     <router-view />
   </div>
 </template>
@@ -13,23 +11,4 @@ export default {
 };
 </script>
 
-<style>
-@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@600;900&display=swap");
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-  text-decoration: none;
-  font-family: "Poppins", sans-serif;
-}
-html {
-  background: linear-gradient(rgba(22, 22, 22, 0.76), rgba(22, 22, 22, 0.76)),
-    url("./assets/Login_image.jpg");
-  background-size: cover;
-  background-attachment: fixed;
-  background-position: center;
-}
-.container {
-  margin: 3rem;
-}
-</style>
+
