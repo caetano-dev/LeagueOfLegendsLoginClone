477f6f430baeee2f7c829049cc51941dc5a7a61a
Pedro Pires <pcaetanop@gmail.com>
2020-12-13 13:52:00 -0300

    Building login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="about">
-    <h1>Login page being built</h1>
+  <div class="Container">
+    <Sidebar/>
   </div>
 </template>
+
+<script>
+import Sidebar from "../components/Sidebar";
+export default {
+  name: "App",
+  components: {
+    Sidebar,
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@600;900&display=swap");
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  font-family: "Poppins", sans-serif;
+}
+.Container {
+  background:url("../assets/Login_image.jpg");
+  background-size: cover;
+  background-attachment: fixed; 
+  background-position: center;
+}
+</style>
